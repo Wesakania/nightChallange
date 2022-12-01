521cb7769778d957a2871adabfbd6bad0ccf6d4a
Wesakania <wesakaniadavis@gmail.com>
2022-12-01 21:44:04 +0300

    chnages

--- night.js
+++ night.js
@@ -1,4 +1,19 @@
-function greetings(){
+/*function greetings(){
   alert("hello world");
 }
-greetings();
+greetings();*/
+
+function clubing(){
+  let age = prompt("How old are you?");
+  let outing = age;
+  if(outing <=18){
+    alert("What the fack are you doing here?");
+  }else if(outing <= 25){
+    alert("Did you ask permission from your parents?");
+  }else if(outing <=30){
+    alert("When are you getting mariied?");
+  }else{
+    return "You can join the club";
+  }
+}
+clubing();
